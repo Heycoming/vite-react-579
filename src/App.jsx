09fa4838d0bbdd72fe31b99e5b465b2c0efb6443
Heycoming <lubing@umich.edu>
2026-01-10 23:53:06 -0500

    React Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
 // src/App.jsx
-import { Routes, Route } from 'react-router-dom';
-// ... 引入 Header, Footer ...
-import Home from './pages/Home';
-import About from './pages/About';          // <--- 确保引入
-import NorthMichigan from './pages/NorthMichigan';
-import Reminder from './pages/Reminder';    // <--- 确保引入
+import { Routes, Route, Outlet } from "react-router-dom";
 
-function App() {
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NorthMichigan from "./pages/NorthMichigan";
+import Reminder from "./pages/Reminder";
+
+// 全站通用布局：固定头尾 + 中间内容区
+function Layout() {
   return (
     <div className="app-container">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />          {/* <--- 路由配置 */}
-        <Route path="/north-michigan" element={<NorthMichigan />} />
-        <Route path="/reminder" element={<Reminder />} />    {/* <--- 路由配置 */}
-      </Routes>
+      <main className="app-main">
+        <Outlet />
+      </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      {/* 需要 Header/Footer 的页面都放到这个 Layout 下面 */}
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/north-michigan" element={<NorthMichigan />} />
+        <Route path="/reminder" element={<Reminder />} />
+      </Route>
+
+      {/* 以后如果有“不需要头尾”的页面，就放在 Layout 外面 */}
+      {/* <Route path="/login" element={<Login />} /> */}
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+  );
+}
